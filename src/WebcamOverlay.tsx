3bf68e178f022a4e70ebf36fc73f96a085db6150
Philipp Lehmann <philipp@redefine.studio>
2023-09-08 09:53:33 +0200

    circular mask

--- src/WebcamOverlay.tsx
+++ src/WebcamOverlay.tsx
@@ -88,7 +88,9 @@ export const WebcamOverlay = () => {
     canvas.width = 480;
     canvas.height = 480;
 
+
     image.onload = () => {
+      const cornerSize = 240; // Set the corner size to 50px
       const x = (canvas.width - image.width) / 2;
       const y = (canvas.height - image.height) / 2;
 
@@ -96,16 +98,71 @@ export const WebcamOverlay = () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.drawImage(image, x, y);
 
+      // Create rounded corners
+      ctx.fillStyle = "white"; // Fill the entire canvas with white
+
+      // Top-left corner
+      ctx.beginPath();
+      ctx.moveTo(0, 0);
+      ctx.lineTo(0, cornerSize);
+      ctx.arcTo(0, 0, cornerSize, 0, cornerSize);
+      ctx.closePath();
+      ctx.fill();
+
+      // Top-right corner
+      ctx.beginPath();
+      ctx.moveTo(canvas.width, 0);
+      ctx.lineTo(canvas.width - cornerSize, 0);
+      ctx.arcTo(canvas.width, 0, canvas.width, cornerSize, cornerSize);
+      ctx.closePath();
+      ctx.fill();
+
+      // Bottom-left corner
+      ctx.beginPath();
+      ctx.moveTo(0, canvas.height);
+      ctx.lineTo(0, canvas.height - cornerSize);
+      ctx.arcTo(0, canvas.height, cornerSize, canvas.height, cornerSize);
+      ctx.closePath();
+      ctx.fill();
+
+      // Bottom-right corner
+      ctx.beginPath();
+      ctx.moveTo(canvas.width, canvas.height);
+      ctx.lineTo(canvas.width - cornerSize, canvas.height);
+      ctx.arcTo(canvas.width, canvas.height, canvas.width, canvas.height - cornerSize, cornerSize);
+      ctx.closePath();
+      ctx.fill();
+
+      // Convert the canvas to grayscale
+      ctx.fillStyle = "rgba(0, 0, 0, 1)"; // Black
+      ctx.globalCompositeOperation = "saturation";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      // Apply a posterization effect to the canvas
+      const levels = 4; // Adjust the number of levels as needed
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      const data = imageData.data;
+
+      for (let i = 0; i < data.length; i += 4) {
+        data[i] = Math.floor((data[i] / 255) * levels) * (255 / levels);
+        data[i + 1] = Math.floor((data[i + 1] / 255) * levels) * (255 / levels);
+        data[i + 2] = Math.floor((data[i + 2] / 255) * levels) * (255 / levels);
+      }
+
+      ctx.putImageData(imageData, 0, 0);
+
       // Add a bright red square for debugging
       ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
       ctx.fillRect(0, 0, 100, 100);
 
-      // Capture image data after drawing
-      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      // Capture image data after drawing and applying filters
+      const finalImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
       // Convert the canvas to SVG with the captured image data
-      convertCanvasToSVG(imageData);
+      convertCanvasToSVG(finalImageData);
     };
+
+
 
     image.onerror = (error) => {
       console.error("Image loading error:", error);
@@ -117,17 +174,16 @@ export const WebcamOverlay = () => {
   };
 
   // Function to convert the canvas content to SVG using imagetracerjs
-  const convertCanvasToSVG = (imageData) => {
+  const convertCanvasToSVG = (imageData: ImageData) => {
     const canvas = canvasRef.current;
     if (!canvas) return;
 
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
 
-    console.log("Captured Image Data:", imageData); // Log the captured image data
-
     try {
-      const tracedSVG = imagetracer.imagedataToSVG(imageData);
+      const options = { ltres: 0.2, qtres: 1.01, pathomit: 10 }; //
+      const tracedSVG = imagetracer.imagedataToSVG(imageData, null, options);
       setSvgData(tracedSVG); // Set the SVG data to state
       console.log(tracedSVG);
     } catch (error) {
